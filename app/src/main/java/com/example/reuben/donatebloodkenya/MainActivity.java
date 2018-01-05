b2ce700d254df52a7bb8a2b61b135687f0e3de17
reuben <rbichage6@gmail.com>
2018-01-05 18:41:37 +0300

    more changes

--- app/src/main/java/com/example/reuben/donatebloodkenya/MainActivity.java
+++ app/src/main/java/com/example/reuben/donatebloodkenya/MainActivity.java
@@ -1,19 +1,40 @@
 package com.example.reuben.donatebloodkenya;
 
+import android.animation.Animator;
 import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import java.util.concurrent.Delayed;
 
 public class MainActivity extends AppCompatActivity {
     public  static  int SPLASH_TIMEOUT =4000;
+    ImageView iv_launcher;
+    ProgressBar progressBar;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //TODO-App controller
+        iv_launcher = findViewById(R.id.iv_launcher);
+        progressBar = findViewById(R.id.progressBar);
+        textView = findViewById(R.id.tvwelcome);
+
+        Animation launcher =  AnimationUtils.loadAnimation(this, R.anim.launcher_transition);
+            iv_launcher.startAnimation(launcher);
+            progressBar.startAnimation(launcher);
+            textView.startAnimation(launcher);
+
+
+
 
         new Handler().postDelayed(new Runnable() {
             @Override
@@ -23,5 +44,8 @@ public class MainActivity extends AppCompatActivity {
                 finish();
             }
         }, SPLASH_TIMEOUT);
+
+
+
     }
 }
